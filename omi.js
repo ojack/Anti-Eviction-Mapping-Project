dbb66b1185eba388fd6cc31dddeb41d52ff7e466
ojack <ojackson@gmail.com>
2013-07-28 15:20:17 -0700

    added animated files

---
+++ omi.js
@@ -0,0 +1,183 @@
+
+//var CartoDB = Backbone.CartoDB({ user: 'viz2' });
+var CartoDB = Backbone.CartoDB({ user: 'ojack' });
+
+var EarthQuake = CartoDB.CartoDBModel.extend({
+
+  //ANIMATION_TIME: 3600*4*1000,
+  ANIMATION_TIME: 3600*4*1000*200*5,
+
+  getPos: function() {
+    var coords = this.get('position').coordinates;
+    return new MM.Location(coords[1], coords[0]);
+  },
+
+  isActive: function(t) {
+      var dt = t - this.time.getTime();
+      //return dt > 0 && dt < this.ANIMATION_TIME;
+      return dt > 0 && dt < this.ANIMATION_TIME;
+  },
+
+  scaleAt: function(t) {
+      var dt = t - this.time.getTime();
+      var interpol_time = this.ANIMATION_TIME;
+      if(dt > 0 && dt < interpol_time) {
+          var tt = this.scale*dt/interpol_time;
+          var r = 1 + 20* Math.sqrt(tt);
+          return r;
+      }
+      return 0;
+  },
+
+  opacity: function(t) {
+      var dt = t - this.time.getTime();
+      var interpol_time = this.ANIMATION_TIME*1.2;
+      if(dt > 0 && dt < interpol_time) {
+          var a= (1 - dt/interpol_time);
+          //return Math.max(0, a*a)*0.3;
+          return Math.max(0, a*a)*0.8;
+      }
+      return 0.0;
+  }
+
+});
+
+var EarthQuakes = CartoDB.CartoDBCollection.extend({
+
+  initialize: function(vehicle) {
+    _.bindAll(this, 'transform');
+    this.bind('reset', this.transform);
+  },
+
+  // transform the data and prepare some needs interpolations
+  transform: function() {
+    this.each(function(m) {
+      m.time = new Date(m.get('timestamp'));
+    });
+
+    this.each(function(m) {
+      //m.scale = parseFloat(m.get('magnitude'));
+      m.scale = 8
+      ;
+    });
+  },
+
+  getActiveEarthquakes: function(t) {
+      var active = [];
+      this.each(function(m) {
+          if(m.isActive(t)) {
+              active.push({ 'id': m.id , 'data': m });
+          }
+      });
+      return active;
+  },
+
+
+
+  model: EarthQuake, 
+  table: 'owner_move_in', //public table
+  columns: {
+      'timestamp': 'date_filed',
+      'position': 'the_geom',
+      //'magnitude': 'magnitude',
+      'id': 'cartodb_id'
+  },
+  order: 'date_filed'
+
+});
+
+
+/*
+ * animated overlay
+ */
+function Overlay(map, earthquakes) {
+
+    this.earthquakes = earthquakes;
+    this.time = earthquakes.first().time.getTime();
+
+    this.div = document.createElement('div');
+    this.div.style.position = 'absolute';
+    this.div.style.width =  map.dimensions.x + "px";
+    this.div.style.height = map.dimensions.y + "px";
+    map.parent.appendChild(this.div);
+    this.svg = d3.select(this.div).append("svg:svg")
+           .attr("width",  map.dimensions.x)
+           .attr("height", map.dimensions.y);
+
+    var self = this;
+    var callback = function(m, a) { 
+      return self.draw(m); 
+    };
+    map.addCallback('drawn', callback);
+    this.draw(map);
+
+}
+
+Overlay.prototype = {
+  draw: function(map) {
+    var self = this;
+    var node = this.svg.selectAll("g")
+          .data(this.earthquakes.getActiveEarthquakes(this.time), function(d) { return d.id; })
+            .attr('transform', function(val) {
+                  var eq = val.data;
+                  var p = eq.getPos(self.time);
+                  p = map.coordinatePoint(map.locationCoordinate(p));
+                  return "translate(" + p.x + "," + p.y +")";
+             })
+          .enter()
+            .append('g')
+            .attr('transform', function(val) {
+                  var eq = val.data;
+                  var p = eq.getPos(self.time);
+                  p = map.coordinatePoint(map.locationCoordinate(p));
+                  return "translate(" + p.x + "," + p.y +")";
+          });
+    node.append("circle")
+      .attr('style', "stroke: #FFF; fill: #F00; fill-opacity: 0.3");
+
+    this.svg.selectAll('g').selectAll('circle')
+      .attr("r", function(b) {
+        return b.data.scaleAt(self.time);
+      })
+      .attr('style', function(b) {
+        var o = b.data.opacity(self.time);
+        return "stroke: #FFF; fill: #F00; fill-opacity: " + o + "; stroke-opacity: " + o;
+      });
+  }
+}
+
+
+function initMap() {
+    var map;
+
+
+    // create map
+    var src = document.getElementById('src');
+    template = 'https://maps.nlp.nokia.com/maptiler/v2/maptile/newest/normal.day/{Z}/{X}/{Y}/256/png8?lg=eng&token=61YWYROufLu_f8ylE0vn0Q&app_id=qIWDkliFCtLntLma2e6O';
+    var subdomains = [ '', 'a.', 'b.', 'c.' ];
+    var provider = new MM.TemplatedLayer(template, subdomains);
+
+    map = new MM.Map(document.getElementById('map'), provider);
+
+    var earthquakes = new EarthQuakes();
+    var setup_layer = function() {
+      var f = new Overlay(map, earthquakes);
+      setInterval(function() {
+       // f.time += 300000; 
+        f.time += 1000000*20*5; 
+        f.draw(map);
+        document.getElementById('date').innerHTML = new Date(f.time).getUTCFullYear().toString();
+        //.replace(/GMT.*/g,'');
+      },20);
+    };
+
+    // fetch all data
+    earthquakes.bind('reset', setup_layer);
+    earthquakes.fetch();
+    if(!location.hash) {
+         map.setCenterZoom(new MM.Location(37.75, -122.45), 13);
+    }
+    var hash = new MM.Hash(map);
+
+
+}
